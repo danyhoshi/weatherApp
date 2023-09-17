d39bc117abf306864b0845b2f8f43f5fd0bf366b
dgonzalez777 <1989.daniela@gmail.com>
2023-09-17 17:22:25 -0400

    Add some styles for component DayCard

---
+++ src/components/DayCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function DayCard() {
+  return (
+    <div className='containerDay'>
+        <h2 className='place'>Maracay</h2>
+        <h3 className='day'>Sunday</h3>
+        <div className='containerIcon'>
+            <img src="" alt="forecast" style={ {width:"100", height: "100"}}/>
+        </div>
+        <p className='tempMaxMin'>tempMax°C \u00A0 \u00A0 tempMin °C </p>
+        <p className='date'>17/09/2023</p>
+    </div>
+  )
+}
+
+export default DayCard
